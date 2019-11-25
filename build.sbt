ec0a5cca9a49d1f65115e0661562c4638e7ed854
luckyshroom <artemletter@gmail.com>
2019-11-25 22:47:16 +0700

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "Spark ML"
+
+version := "1.0"
+
+scalaVersion := "2.12.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.4.4"
---
+++ src/main/scala/io/depa/predict/Iris.scala
@@ -0,0 +1,64 @@
+package io.depa.predict
+
+import org.apache.spark.mllib.classification.{LogisticRegressionModel, LogisticRegressionWithLBFGS}
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.stat.Statistics
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Iris {
+
+  def main(args: Array[String]) {
+
+    val sc = SparkContext.getOrCreate(new SparkConf().setAppName("Main").setMaster("local[2]"))
+
+    val data: RDD[String] = sc.textFile("data/mllib/iris_dataset.csv")
+
+    val inputData = data.map(_.split(","))
+      .map(arr => arr.slice(0, arr.length - 1))
+      .map(for (e <- _) yield e.toDouble)
+      .map(Vectors.dense)
+
+    val labelMap = Map("setosa" -> 0, "versicolor" -> 1, "virginica" -> 2)
+
+    val labeledData = data.map(_.split(",")).map(arr => {
+      val label = arr(arr.length - 1)
+      val vector = Vectors.dense(for (e <- arr.slice(0, arr.length - 1)) yield e.toDouble)
+      LabeledPoint(labelMap(label), vector)
+    })
+
+    val summary = Statistics.colStats(inputData)
+    println("Summary Mean:")
+    println(summary.mean)
+    println("Summary Variance:")
+    println(summary.variance)
+    println("Summary Non-zero:")
+    println(summary.numNonzeros)
+
+    val correlation = Statistics.corr(inputData, "pearson")
+    println("Correlation Matrix:")
+    println(correlation.toString)
+
+    val splits = labeledData.randomSplit(Array(0.8, 0.2), 11L)
+    val testData = splits(0)
+    val trainingData = splits(1)
+
+    val model = new LogisticRegressionWithLBFGS()
+      .setNumClasses(3)
+      .run(trainingData)
+
+    val predictionAndLabels = testData.map(p => (model.predict(p.features), p.label))
+    val metrics = new MulticlassMetrics(predictionAndLabels)
+    val accuracy = metrics.accuracy
+    System.out.println("Model Accuracy on Test Data: " + accuracy)
+
+    // Save and evaluate on random data
+    model.save(sc, "model/logistic-regression")
+    val sameModel = LogisticRegressionModel.load(sc, "model/logistic-regression")
+    val newData = Vectors.dense(Array[Double](1, 1, 1, 1))
+    val prediction = sameModel.predict(newData)
+    System.out.println("Model Prediction on New Data = " + prediction)
+  }
+}
